e199f659a2ee2684f7ecb6ce6bbe5566ba3968c3
Anay Wadhera <awadhera@berkeley.edu>
2020-09-04 12:36:40 -0700

    drivers/gpu: import nubia changes

--- drivers/gpu/drm/msm/sde/sde_connector.c
+++ drivers/gpu/drm/msm/sde/sde_connector.c
@@ -70,6 +70,31 @@ static const struct drm_prop_enum_list e_frame_trigger_mode[] = {
 	{FRAME_DONE_WAIT_POSTED_START, "posted_start"},
 };
 
+#ifdef CONFIG_NUBIA_LCD_BACKLIGHT_CURVE
+int nubia_backlight_covert(struct dsi_display *display,
+                                      int value)
+{
+        u32 bl_lvl;
+
+        if(!display){
+                return -EINVAL;
+        }
+
+        pr_debug("before nubia backlight, value = %d\n",value);
+        if(value > 0){
+                bl_lvl =value * (display->panel->bl_config.bl_max_level -display->panel->bl_config.bl_min_level);
+                do_div(bl_lvl,display->panel->bl_config.brightness_max_level);
+                bl_lvl =value *bl_lvl;
+                do_div(bl_lvl,display->panel->bl_config.brightness_max_level);
+                bl_lvl += display->panel->bl_config.bl_min_level;
+        }else{
+                bl_lvl =0;
+        }
+        pr_debug("after nubia backlight, value = %d\n",bl_lvl);
+        return bl_lvl;
+}
+#endif
+
 static int sde_backlight_device_update_status(struct backlight_device *bd)
 {
 	int brightness;
@@ -91,9 +116,13 @@ static int sde_backlight_device_update_status(struct backlight_device *bd)
 	if (brightness > display->panel->bl_config.bl_max_level)
 		brightness = display->panel->bl_config.bl_max_level;
 
+#ifdef CONFIG_NUBIA_LCD_BACKLIGHT_CURVE
+	bl_lvl = nubia_backlight_covert(display,brightness);
+#else
 	/* map UI brightness into driver backlight level with rounding */
 	bl_lvl = mult_frac(brightness, display->panel->bl_config.bl_max_level,
 			display->panel->bl_config.brightness_max_level);
+#endif
 
 	if (!bl_lvl && brightness)
 		bl_lvl = 1;
